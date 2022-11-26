54f144e390a0e7a561f7ca83630266f36ae6a987
calebanderson <caleb.r.anderson.1@gmail.com>
2022-11-25 23:04:03 -0500

    Updated console width setting to be saved as a text file

--- lib/reactive_console.rb
+++ lib/reactive_console.rb
@@ -3,35 +3,46 @@ require "reactive_console/version"
 require "reactive_console/railtie"
 
 module ReactiveConsole
-  CONSOLE_WIDTH_RANGE = (80..800).freeze
-  DEFAULT_CONSOLE_WIDTH = 250
+  class << self
+    CONSOLE_WIDTH_RANGE = (80..800).freeze
+    DEFAULT_CONSOLE_WIDTH = 250
 
-  def width
-    @width ||= DEFAULT_CONSOLE_WIDTH
-  end
+    def width
+      width = width_file.read.to_i
+    rescue Errno::ENOENT, RangeError
+      self.width = DEFAULT_CONSOLE_WIDTH
+      DEFAULT_CONSOLE_WIDTH
+    end
 
-  def config_width(given_width = nil)
-    input_width = given_width || _get_input_width
-    return true if input_width.zero?
+    def width=(val)
+      val = CONSOLE_WIDTH_RANGE.cover?(val.to_i) && val.to_i
+      raise ArgumentError, "width must be in range #{CONSOLE_WIDTH_RANGE}" unless val
+      width_file.open(File::TRUNC | File::CREAT | File::WRONLY) { |f| f << val }
+    end
 
-    if CONSOLE_WIDTH_RANGE.cover?(input_width)
-      @width = input_width
+    def width_file
+      Pathname.new(File.expand_path('../../.width.txt', __FILE__))
+    end
+
+    def config_width(given_width = nil)
+      input_width = given_width || _get_input_width
+      return true if input_width.zero?
+
+      self.width = input_width
       print('New ') & display_width if given_width.nil?
       true
-    else
-      puts "Width #{input_width} not within accepted range (#{CONSOLE_WIDTH_RANGE})"
     end
-  end
 
-  def _get_input_width
-    display_width
-    puts 'Press enter/return to accept. Enter a width or characters up to the desired limit, then press enter to set.'
-    input = gets.chomp
-    input.size < CONSOLE_WIDTH_RANGE.min && input[/^\d+$/] ? input.to_i : input.size
-  end
+    def _get_input_width
+      display_width
+      puts 'Press enter/return to accept. Enter a width or characters up to the desired limit, then press enter to set.'
+      input = gets.chomp
+      input.size < CONSOLE_WIDTH_RANGE.min && input[/^\d+$/] ? input.to_i : input.size
+    end
 
-  def display_width
-    puts "Console Width: #{width}"
-    puts '-' * width.to_i
+    def display_width
+      puts "Console Width: #{width}"
+      puts '-' * width.to_i
+    end
   end
 end
