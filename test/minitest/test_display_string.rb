75a90744f621d45de80e3ea1907da7050b0b1ac8
calebanderson <caleb.r.anderson.1@gmail.com>
2023-01-29 23:18:54 -0500

    Added hash tests and rewrote shared testing module

--- test/minitest/test_display_string.rb
+++ test/minitest/test_display_string.rb
@@ -2,21 +2,75 @@ require_relative '../minitest_helper'
 
 module ResponsiveConsole
   module TestDisplayString
-    def setup
-      HashFormatter::FORMATS[:test] = "%k:\n  %V\n%k:\n  %V"
-      ResponsiveConsole.config_width(@width ||= 80)
+    attr_writer :width, :lengths
+    DEFAULT_WIDTH = 80
+    DEFAULT_LENGTHS = 28..34
+    ARRAY_FORMATS = {
+      test_short: "[ %E, %E ]",
+      test_long: "-- %E\n-- %E",
+      test: :test_short
+    }
+    HASH_FORMATS = {
+      test_short: '{ %k: %V, %k: %V }',
+      test_long: "{\n  %k:  %V,\n  %k:  %V\n}",
+      test: :test_short
+    }
 
-      @chars = [*'A'..'Z', *'a'..'z']
-      elements = padded_chars(@range ||= 28..34)
-      @nested_array = 3.times.map { 8.times.map { elements.shift } }
-      @array_values = @nested_array.deep_dup.index_by(&:shift)
-      @array_keys = @array_values.deep_dup.invert
+    def setup
+      ResponsiveConsole.config_width(width)
+      ArrayFormatter::FORMATS.merge!(ARRAY_FORMATS) { |_, orig, _| orig }
+      HashFormatter::FORMATS.merge!(HASH_FORMATS) { |_, orig, _| orig }
     end
 
-    def padded_chars(size_range)
+    def teardown
+      ArrayFormatter::FORMATS.delete(:test)
+      HashFormatter::FORMATS.delete(:test)
+    end
+
+    def width
+      @width ||= DEFAULT_WIDTH
+    end
+
+    def lengths
+      @lengths ||= DEFAULT_LENGTHS
+    end
+
+    def array_format=(val)
+      ArrayFormatter::FORMATS[:test] = val
+    end
+
+    def hash_format=(val)
+      HashFormatter::FORMATS[:test] = val
+    end
+
+    def padded_chars(size_range = lengths)
       size_range = size_range..size_range unless size_range.is_a?(Range)
       sizes = size_range.cycle
-      @chars.map { |c| format("%-#{sizes.peek}.#{sizes.next}s", c).gsub(' ', '_') }
+      [*'A'..'Z', *'a'..'z'].map { |c| "#{c}#{'~' * (sizes.next - 1)}" }
+    end
+
+    def nested_array
+      padded_chars.first(12).each_slice(4).to_a
+    end
+
+    def array_values
+      nested_array.index_by(&:shift)
+    end
+
+    def array_keys
+      array_values.invert
+    end
+
+    def hash_elements
+      nested_array.map { |el| el.each_slice(2).to_h }
+    end
+
+    def hash_values
+      hash_elements.index_by { |h| h.keys.last }
+    end
+
+    def hash_keys
+      hash_values.invert
     end
 
     def rstrip_lines(text)
@@ -25,11 +79,7 @@ module ResponsiveConsole
 
     def subject_output(object)
       output = DisplayString.new(object).output(:test)
-      rstrip_lines(output).tap { |o| puts o }
-    end
-
-    def test_existence
-      refute_nil defined?(DisplayString)
+      rstrip_lines(output).tap { |o| puts [o, nil] }
     end
   end
 end
